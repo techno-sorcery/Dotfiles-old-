1789dd541becbc62d1446d449d31248eba37e348
Hayden Buscher <inquisitivecalculator@gmail.com>
2023-01-14 15:35:05 -0800

    Update

--- install.sh
+++ install.sh
@@ -4,13 +4,16 @@
 ./apt.sh
 
 #Folders
-mkdir ~/.Xmerge
 mkdir Documents
 mkdir Downloads
 mkdir Github
 mkdir Music
 mkdir Photos
 mkdir Videos
+
+mkdir ~/.Xmerge
+mkdir .config/autorandr/docked
+mkdir .config/autorandr/mobile
 
 #Symlinks
 ./symlink.sh
--- symlink.sh
+++ symlink.sh
@@ -7,5 +7,8 @@ ln -sf ~/Dotfiles/xinitrc ~/.xinitrc
 ln -sf ~/Dotfiles/digital ~/.digital.cfg
 #sudo ln -sf ~/Dotfiles/tlp /etc/tlp.conf
 
+ln -sf ~/Dotfiles/autorandr/docked/* ~/.config/autorandr/docked
+ln -sf ~/Dotfiles/autorandr/mobile/* ~/.config/autorandr/mobile
+
 #Folders
 ln -sf ~/Dotfiles/Xmerge/* ~/.Xmerge/
